00f55795fbadaace5a32dd270259299cb3b6e847
zhehar17 <123106092+zhehar17@users.noreply.github.com>
2026-03-30 21:49:59 -0700

    Implement getComputerChoice
    Use Math.random() with floor() to get integer for array index.
    floor() is necessary to get number between 0 and 2.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
